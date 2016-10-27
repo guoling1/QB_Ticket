0b6296f4f5a239cdeb6876a01d7febe87c28c8ba
Administrator <706326182@qq.com>
2016-10-27 20:20:47 +0800

    乘客信息的增删改查数据库接口及车站、车次参数封装

---
+++ src/main/java/com/jkm/entity/ContactForm.java
@@ -0,0 +1,42 @@
+package com.jkm.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ContactForm extends BaseEntity {
+    /**
+     * 用户id
+     */
+    private long userId;
+
+    /**
+     * 用户姓名
+     */
+    private String userName;
+
+    /**
+     * 证件类型
+     */
+    private int cardType;
+
+    /**
+     * 证件号
+     */
+    private String cardNo;
+
+    /**
+     * 用户电话
+     */
+    private String userPhone;
+
+    /**
+     * 旅客类型
+     */
+    private int userType;
+
+}
