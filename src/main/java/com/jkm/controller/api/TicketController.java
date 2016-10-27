3a352d00d767317b65a9d254033b8088faec4b66
xiang.yu <http://gitlab.jinkaimen.com>
2016-10-27 14:09:55 +0800

    controller

--- src/main/java/com/jkm/controller/api/TicketController.java
+++ src/main/java/com/jkm/controller/api/TicketController.java
@@ -11,13 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * Created by yuxiang on 2016-10-27.
  */
 @RequestMapping("/ticket")
-@Controller
+@RestController
 public class TicketController {
 
     private static Logger logger = Logger.getLogger(PacketController.class);
