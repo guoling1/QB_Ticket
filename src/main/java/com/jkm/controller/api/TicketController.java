15e7307845e44e215604aa54ce7941490a4892e9
xiang.yu <http://gitlab.jinkaimen.com>
2016-10-27 14:52:19 +0800

    controller

--- src/main/java/com/jkm/controller/api/TicketController.java
+++ src/main/java/com/jkm/controller/api/TicketController.java
@@ -3,7 +3,6 @@ package com.jkm.controller.api;
 import com.jkm.controller.helper.ResponseEntityBase;
 import com.jkm.controller.helper.request.RequestTicketRefund;
 import com.jkm.controller.helper.response.ResponseTicketRefund;
-import com.jkm.service.JkmPacketGetService;
 import com.jkm.service.TicketService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +10,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * Created by yuxiang on 2016-10-27.
  */
 @RequestMapping("/ticket")
-@RestController
+@Controller
 public class TicketController {
 
     private static Logger logger = Logger.getLogger(PacketController.class);
