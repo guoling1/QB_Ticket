b2a2adcc13380736783dc7e8bf58b9ef020117f2
zhangyulong <yulong.zhang@jinkaimen.com>
2016-10-28 11:09:43 +0800

    test ubg

--- src/test/java/TestMyBatis.java
+++ src/test/java/TestMyBatis.java
@@ -9,16 +9,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml","classpath:spring-mvc.xml",})
+//@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
+//@ContextConfiguration(locations = {"classpath:spring-mybatis.xml","classpath:spring-mvc.xml"})
 
 public class TestMyBatis {
-    private static Logger logger = Logger.getLogger(TestMyBatis.class);
-    @Resource
-    private IUserService iUserService;
-    @Test
-    public void test1() {
-        User user = iUserService.getUserById(1);
-        logger.info(JSON.toJSONString(user));
-    }
+//    private static Logger logger = Logger.getLogger(TestMyBatis.class);
+//    @Resource
+//    private IUserService iUserService;
+//    @Test
+//    public void test1() {
+//        User user = iUserService.getUserById(1);
+//        logger.info(JSON.toJSONString(user));
+//    }
 }
--- src/test/java/TestOrderFormServiceImpl.java
+++ src/test/java/TestOrderFormServiceImpl.java
@@ -16,68 +16,68 @@ import java.util.List;
 /**
  * Created by yulong.zhang on 2016/10/27.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml","classpath:spring-mvc.xml",})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:spring-mybatis.xml","classpath:spring-mvc.xml"})
 public class TestOrderFormServiceImpl {
 
-    @Autowired
-    private OrderFormService orderFormService;
+//    @Autowired
+//    private OrderFormService orderFormService;
 
 
-    @Test
-    public void testAdd() {
-        final OrderForm orderForm = new OrderForm();
-
-        orderForm.setUid(1);
-        orderForm.setQueryKey("YS201610271659123456789");
-        orderForm.setTermTransId("HCP201610271659123456789");
-//        orderForm.setTransId();
-//        orderForm.setPickNo();
-//        orderForm.setInsuranceFee();
-//        orderForm.setTechnicalFee();
-        orderForm.setJourneyType(1);
-        orderForm.setTrainNo("G1001");
-        orderForm.setDepartStationCode("beijingbei");
-        orderForm.setArriveStationCode("nanjingnan");
-        try {
-            orderForm.setDepartDateTime(new SimpleDateFormat("yyy-MM-dd HH:mm").parse("2016-10-28 09:12"));
-            orderForm.setArriveDateTime(new SimpleDateFormat("yyy-MM-dd HH:mm").parse("2016-11-28 09:12"));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        orderForm.setInsureId(0);
-        orderForm.setInsurePrice(new BigDecimal("0.00"));
-        orderForm.setIsMergeNotice(1);
-        orderForm.setIsAcceptNoSeat(0);
-        orderForm.setContactName("于翔");
-        orderForm.setContactMobile("18640426296");
-        orderForm.setStatus(1);
-//        this.orderFormService.add(orderForm);
-    }
-
-    @Test
-    public void testUpdate() {
-        final Optional<OrderForm> orderFormOptional = this.orderFormService.selectById(31);
-        final OrderForm orderForm1 = orderFormOptional.get();
-        orderForm1.setTransId("YS201610271709111111");
-        orderForm1.setPickNo("QP123456789");
-        orderForm1.setInsuranceFee(new BigDecimal("0.00"));
-        orderForm1.setTechnicalFee(new BigDecimal("0.00"));
-        this.orderFormService.update(orderForm1);
-    }
-
-    @Test
-    public void testSelect() {
-        final Optional<OrderForm> optional = this.orderFormService.selectById(31);
-        System.out.println("selectById = " + JsonUtil.toJsonString(optional.get()));
-
-        final Optional<OrderForm> orderFormOptional = this.orderFormService.selectByIdWithLock(31);
-        System.out.println("selectByIdWithLock = " + JsonUtil.toJsonString(orderFormOptional.get()));
-
-        final Optional<OrderForm> orderFormOptional1 = this.orderFormService.selectByTermTransId("HCP201610271659123456789");
-        System.out.println("selectByTermTransId = " + JsonUtil.toJsonString(orderFormOptional1.get()));
-
-        final List<OrderForm> orderForms = this.orderFormService.selectByUid(1);
-        System.out.println("selectByUid = " + JsonUtil.toJsonString(orderForms.get(0)));
-    }
+//    @Test
+//    public void testAdd() {
+//        final OrderForm orderForm = new OrderForm();
+//
+//        orderForm.setUid(1);
+//        orderForm.setQueryKey("YS201610271659123456789");
+//        orderForm.setTermTransId("HCP201610271659123456789");
+////        orderForm.setTransId();
+////        orderForm.setPickNo();
+////        orderForm.setInsuranceFee();
+////        orderForm.setTechnicalFee();
+//        orderForm.setJourneyType(1);
+//        orderForm.setTrainNo("G1001");
+//        orderForm.setDepartStationCode("beijingbei");
+//        orderForm.setArriveStationCode("nanjingnan");
+//        try {
+//            orderForm.setDepartDateTime(new SimpleDateFormat("yyy-MM-dd HH:mm").parse("2016-10-28 09:12"));
+//            orderForm.setArriveDateTime(new SimpleDateFormat("yyy-MM-dd HH:mm").parse("2016-11-28 09:12"));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//        orderForm.setInsureId(0);
+//        orderForm.setInsurePrice(new BigDecimal("0.00"));
+//        orderForm.setIsMergeNotice(1);
+//        orderForm.setIsAcceptNoSeat(0);
+//        orderForm.setContactName("于翔");
+//        orderForm.setContactMobile("18640426296");
+//        orderForm.setStatus(1);
+////        this.orderFormService.add(orderForm);
+//    }
+//
+//    @Test
+//    public void testUpdate() {
+//        final Optional<OrderForm> orderFormOptional = this.orderFormService.selectById(31);
+//        final OrderForm orderForm1 = orderFormOptional.get();
+//        orderForm1.setTransId("YS201610271709111111");
+//        orderForm1.setPickNo("QP123456789");
+//        orderForm1.setInsuranceFee(new BigDecimal("0.00"));
+//        orderForm1.setTechnicalFee(new BigDecimal("0.00"));
+//        this.orderFormService.update(orderForm1);
+//    }
+//
+//    @Test
+//    public void testSelect() {
+//        final Optional<OrderForm> optional = this.orderFormService.selectById(31);
+//        System.out.println("selectById = " + JsonUtil.toJsonString(optional.get()));
+//
+//        final Optional<OrderForm> orderFormOptional = this.orderFormService.selectByIdWithLock(31);
+//        System.out.println("selectByIdWithLock = " + JsonUtil.toJsonString(orderFormOptional.get()));
+//
+//        final Optional<OrderForm> orderFormOptional1 = this.orderFormService.selectByTermTransId("HCP201610271659123456789");
+//        System.out.println("selectByTermTransId = " + JsonUtil.toJsonString(orderFormOptional1.get()));
+//
+//        final List<OrderForm> orderForms = this.orderFormService.selectByUid(1);
+//        System.out.println("selectByUid = " + JsonUtil.toJsonString(orderForms.get(0)));
+//    }
 }
