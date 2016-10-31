719482c71c0e3b541f87a1e5299043602735f67d
xiang.yu <http://gitlab.jinkaimen.com>
2016-10-31 14:21:35 +0800

    aes

---
+++ src/test/java/Test3Des.java
@@ -0,0 +1,25 @@
+import com.jkm.entity.helper.UserBankCardSupporter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by yuxiang on 2016-10-31.
+ */
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml","classpath:spring-mvc.xml"})*/
+public class Test3Des {
+
+    @Test
+    public void test(){
+/*
+
+        final String mobile = UserBankCardSupporter.encryptMobile("111");
+        System.out.print("加密后的手机号为:" + mobile + "end");
+        System.out.print("解密后的手机号为:" + UserBankCardSupporter.decryptMobile("C6zhuB4e8HBhcigonTI5kQ") + "end");
+*/
+
+
+    }
+}
