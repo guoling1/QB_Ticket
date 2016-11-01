1584352e4977bdfcb39854d3d2c079405efc6880
于超 <chao.yu@jinkaimen.com>
2016-11-01 17:21:19 +0800

    完成模糊匹配的部分，性能问题需要考虑

---
+++ src/main/webapp/QB_app/src/components/ticket/TrainMenu.vue
@@ -0,0 +1,102 @@
+<template lang="html">
+  <div class="menu flex-box-column flexBox">
+    <router-view></router-view>
+
+    <div class="bottom">
+      <div class="btn" :class="">
+        <div class="icon icon-time"></div>
+        <p>出发 早→晚</p>
+      </div>
+      <div class="btn" :class="">
+        <div class="icon icon-quick"></div>
+        <p>只看高铁动车</p>
+      </div>
+      <div class="btn" :class="">
+        <div class="icon icon-still"></div>
+        <p>只看有票</p>
+      </div>
+      <div class="btn" :class="">
+        <div class="icon icon-screen"></div>
+        <p>综合筛选</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="babel">
+  export default {
+    name: 'menu',
+    data () {
+      return {
+        pathName: this.$route.name
+      }
+    },
+    watch: {
+      $route: function (val) {
+        this.$data.pathName = val.name;
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .flexItem(@val,@width:0) {
+    box-flex: @val;
+    -webkit-box-flex: @val;
+    -webkit-flex: @val;
+    -ms-flex: @val;
+    flex: @val;
+    width: @width;
+  }
+
+  .menu {
+    .flexItem(1, 100%);
+  }
+
+  .bottom {
+    width: 100%;
+    height: 49px;
+    background-color: #FFF;
+    border-top: 1px solid #ebebeb;
+    .btn {
+      float: left;
+      width: 25%;
+      height: 49px;
+      text-align: center;
+      display: block;
+      .icon {
+        margin: 10px auto 0;
+        &.icon-time {
+          width: 16px;
+          height: 16px;
+          background-image: url("../../assets/time.png");
+          background-size: 16px 16px;
+        }
+        &.icon-quick {
+          width: 19px;
+          height: 15px;
+          background-image: url("../../assets/quick.png");
+          background-size: 19px 15px;
+        }
+        &.icon-still {
+          width: 20px;
+          height: 15px;
+          background-image: url("../../assets/still.png");
+          background-size: 20px 15px;
+        }
+        &.icon-screen {
+          width: 16px;
+          height: 16px;
+          background-image: url("../../assets/screen.png");
+          background-size: 16px 16px;
+        }
+      }
+      p {
+        font-size: 11px;
+        color: #585858;
+        margin-top: 5px;
+      }
+    }
+  }
+</style>
--- src/main/webapp/QB_app/src/main.js
+++ src/main/webapp/QB_app/src/main.js
@@ -1,6 +1,61 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const station_names = require('./station_name');
+
+// 定义全局指令
+// 全局火车站搜索指令
+Vue.directive('search-station', {
+  inserted: function (el) {
+    // 车站信息解析
+    const stationList = {};
+    for (let i = 0; i < station_names.split('@').length; i++) {
+      let parsing_middleware = station_names.split('@')[i].split('|');
+      stationList[parsing_middleware[5]] = {};
+      stationList[parsing_middleware[5]].city = parsing_middleware[1];
+      stationList[parsing_middleware[5]].fullname = parsing_middleware[3];
+      stationList[parsing_middleware[5]].shortname = parsing_middleware[4];
+      stationList['length'] = i;
+    }
+    // 添加 搜索input 和 模糊匹配显示区ul
+    var input = document.createElement('input');
+    var ul = document.createElement('ul');
+    input.addEventListener('input', function () {
+      let val = input.value;
+      let matchList = [];
+      // 根据用户输入,进行模糊匹配
+      // matchList里只记录id,完成后去重
+      for (let i = 0; i < stationList.length; i++) {
+        if (stationList[i].city.indexOf(val) > -1) {
+          matchList.push(stationList[i].city);
+          continue;
+        }
+        if (stationList[i].fullname.indexOf(val) > -1) {
+          matchList.push(stationList[i].city);
+          continue;
+        }
+        if (stationList[i].shortname.indexOf(val) > -1) {
+          matchList.push(stationList[i].city);
+        }
+      }
+      // 根据匹配结果添加匹配列表
+      ul.innerHTML = '';
+      if (matchList.length == 0) {
+        var empty = document.createElement('li');
+        empty.innerHTML = '啥也没搜到';
+        ul.appendChild(empty);
+      }
+      for (let i = 0; i < matchList.length; i++) {
+        var li = document.createElement('li');
+        li.innerHTML = matchList[i];
+        ul.appendChild(li);
+      }
+      el.appendChild(ul);
+    });
+    el.appendChild(input);
+  }
+});
+
 // 0. 如果使用模块化机制编程， 要调用 Vue.use(VueRouter)
 Vue.use(VueRouter);
 
@@ -12,7 +67,7 @@ import App from './App';
 const ticketMainMenu = r => require.ensure([], () => r(require('./components/ticket/MainMenu')), 'group-ticket');
 const ticketReserve = r => require.ensure([], () => r(require('./components/ticket/Reserve')), 'group-ticket');
 const ticketOrder = r => require.ensure([], () => r(require('./components/ticket/Order')), 'group-ticket');
-const ticketChooseMenu = r => require.ensure([], () => r(require('./components/ticket/ChooseMenu')), 'group-ticket');
+const ticketTrainMenu = r => require.ensure([], () => r(require('./components/ticket/TrainMenu')), 'group-ticket');
 const ticketTrain = r => require.ensure([], () => r(require('./components/ticket/Train')), 'group-ticket');
 // 支付 组件 紧密关联账户组件,注意留下接口,方便修改
 // 按需加载 1,组件单独打包
@@ -54,9 +109,9 @@ const routes = [
         ]
       },
       {
-        path: 'choose-menu',
-        redirect: '/ticket/choose-menu/train',
-        component: ticketChooseMenu,
+        path: 'train-menu',
+        redirect: '/ticket/train-menu/train',
+        component: ticketTrainMenu,
         children: [
           {
             path: 'train',
