4bee74268afa5e8e8065b92ae0e19ba4c1844734
于超 <chao.yu@jinkaimen.com>
2016-11-01 10:03:56 +0800

    vue 3层架构 完成 顶部title决定下方内容，底部menu决定中间主要内容

---
+++ src/main/webapp/QB_app/src/components/ticket/Menu.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div class="menu flex-box-column flexBox">
+    <router-view></router-view>
+
+    <div class="bottom">
+      <div class="btn">车票预定</div>
+      <div class="btn">我的订单</div>
+    </div>
+  </div>
+</template>
+
+<script lang="babel">
+  export default {
+    name: 'menu',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .flexItem(@val,@width:0) {
+    box-flex: @val;
+    -webkit-box-flex: @val;
+    -webkit-flex: @val;
+    -ms-flex: @val;
+    flex: @val;
+    width: @width;
+  }
+
+  .menu {
+    .flexItem(1, 100%);
+  }
+
+  .bottom {
+    width: 100%;
+    height: 50px;
+    .btn {
+      float: left;
+      width: 50%;
+      height: 50px;
+      line-height: 50px;
+      text-align: center;
+    }
+  }
+</style>
--- src/main/webapp/QB_app/src/main.js
+++ src/main/webapp/QB_app/src/main.js
@@ -6,32 +6,62 @@ Vue.use(VueRouter);
 
 // 1. 定义（路由）组件。
 // 可以从其他文件 import 进来
+// !* 更棒的是,采用按需加载的方式
 import App from './App';
-import Hello from './components/Hello';
-
-const Foo = {template: '<div>Foo</div>'};
-const Default = {template: '<div>车票预定</div>'};
+// 应用 火车票 流程组件
+const ticketMenu = r => require.ensure([], () => r(require('./components/ticket/Menu')), 'group-ticket');
+const ticketMain = r => require.ensure([], () => r(require('./components/ticket/Main')), 'group-ticket');
+// 支付 组件 紧密关联账户组件,注意留下接口,方便修改
+// 按需加载 1,组件单独打包
+// const payMenu = resolve => require(['./components/pay/Menu'], resolve);
+// 按需加载 2.组件分组打包
+const payMenu = r => require.ensure([], () => r(require('./components/pay/Menu')), 'group-pay');
+const payMain = r => require.ensure([], () => r(require('./components/pay/Main')), 'group-pay');
 
 // 2. 定义路由
-// 每个路由应该映射一个组件。 其中"component" 可以是
-// 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
 // 这里我们将App作为顶级路由,其他采用嵌套路由。
+// 充分利用嵌套路由
+// 1级作为顶部路由,2级作为底部路由,3级作为主内容区路由
+// !* 赋予 命名路由 单独作用 : 用于路径判断,以显示顶部title
 const routes = [
   {
-    path: '/', component: App,
+    path: '/',
+    redirect: '/ticket'
+  },
+  {
+    path: '/ticket',
+    redirect: '/ticket/menu/main',
+    component: App,
     children: [
       {
-        path: '',
-        component: Default
-      },
+        path: 'menu',
+        redirect: '/ticket/menu/main',
+        component: ticketMenu,
+        children: [
+          {
+            path: 'main',
+            name: 'ticketReservation',
+            component: ticketMain
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/pay',
+    redirect: '/pay/menu/main',
+    component: App,
+    children: [
       {
-        path: 'bar',
-        component: Hello
-      },
-      {
-        path: 'foo',
-        component: Foo
+        path: 'menu',
+        redirect: '/pay/menu/main',
+        component: payMenu,
+        children: [
+          {
+            path: 'main',
+            component: payMain
+          }
+        ]
       }
     ]
   }
@@ -40,7 +70,11 @@ const routes = [
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
+  mode: 'history',
+  routes, // （缩写）相当于 routes: routes
+  scrollBehavior (to, from, savedPosition) {
+    // return 期望滚动到哪个的位置
+  }
 });
 
 // 4. 创建和挂载根实例。
