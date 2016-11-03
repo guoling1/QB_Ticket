c54d096cccb74572acde504359782f1f6dc18b5f
于超 <chao.yu@jinkaimen.com>
2016-11-03 20:17:47 +0800

    接口调试

--- src/main/webapp/QB_app/src/components/ticket/Train.vue
+++ src/main/webapp/QB_app/src/components/ticket/Train.vue
@@ -2,9 +2,10 @@
   <div class="main">
     <div class="banner">
       <span class="first">前一天</span>
+
       <div class="calendar">
         10月31日 周五
-        <img src="../../assets/calendar.png" alt="" />
+        <img src="../../assets/calendar.png" alt=""/>
       </div>
       <span class="next show">后一天</span>
     </div>
@@ -12,6 +13,7 @@
       <li>
         <div class="top">
           <span class="checi">G307</span>
+
           <div class="topRight">
             <p>￥850<span>起</span></p>
             <span class="time">7小时25分</span>
@@ -20,7 +22,7 @@
             <div class="midTop">
               <span class="form">北京西</span>
               <span class="icon"></span>
-              <img src="../../assets/jiantou.png" alt="" />
+              <img src="../../assets/jiantou.png" alt=""/>
               <span class="to">深圳</span>
             </div>
             <div class="midBottom">
@@ -33,6 +35,7 @@
           <p>
             二等座:<span class="num">12</span>张
           </p>
+
           <p>
             二等座:<span class="num">12</span>张
           </p>
@@ -41,6 +44,7 @@
       <li class="select">
         <div class="top">
           <span class="checi">G307</span>
+
           <div class="topRight">
             <p>￥850<span>起</span></p>
             <span class="time">7小时25分</span>
@@ -49,7 +53,7 @@
             <div class="midTop">
               <span class="form">北京西</span>
               <span class="icon"></span>
-              <img src="../../assets/jiantou.png" alt="" />
+              <img src="../../assets/jiantou.png" alt=""/>
               <span class="to">深圳</span>
             </div>
             <div class="midBottom">
@@ -62,6 +66,7 @@
           <p>
             二等座:<span class="num">12</span>张
           </p>
+
           <p>
             二等座:<span class="num">12</span>张
           </p>
@@ -73,12 +78,26 @@
 </template>
 
 <script lang="babel">
+  import Vue from 'vue'
   export default {
     name: 'menu',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App'
       }
+    },
+    beforeRouteEnter (to, from, next) {
+      Vue.http.post('/queryTicketPrice/query', {
+        from_station: 'VAP',
+        to_station: 'VNP',
+        train_date: '2016-12-06'
+      }).then(function (response) {
+        console.log(response);
+        next();
+      }, function (response) {
+        console.log(response);
+        next();
+      })
     }
   }
 </script>
@@ -101,32 +120,32 @@
     background: #f6f6f6;
     .flexItem(1, 100%);
 
-    .banner{
-      padding:9px 20px;
+    .banner {
+      padding: 9px 20px;
       height: 45px;
       width: 100%;
       background: #4ab9f1;
 
-      .first,.next{
-        height:27px;
+      .first, .next {
+        height: 27px;
         line-height: 27px;
         font-size: 12px;
         color: #91daff;
         float: left;
       }
 
-      .next{
+      .next {
         float: right;
 
       }
 
-      .show{
+      .show {
         color: #fff;
       }
 
-      .calendar{
+      .calendar {
         display: inline-block;
-        border:1px solid #84d0f6;
+        border: 1px solid #84d0f6;
         border-radius: 2px;
         width: 124px;
         height: 26px;
@@ -135,7 +154,7 @@
         font-size: 12px;
         overflow: hidden;
 
-        img{
+        img {
           width: 16px;
           height: 16px;
           vertical-align: -3px;
@@ -143,61 +162,61 @@
         }
       }
     }
-    ul li{
+    ul li {
       margin-top: 5px;
-      padding:15px;
+      padding: 15px;
       height: 101px;
       width: 100%;
       background: #fff;
 
-      &.select{
+      &.select {
         background: #f6f6f6;
       }
 
-      .top{
+      .top {
         margin-bottom: 17px;
         height: 34px;
         line-height: 34px;
 
-        .checi{
+        .checi {
           float: left;
           font-size: 22px;
         }
 
-        .topRight{
+        .topRight {
           float: right;
-          margin:0 0 17px 0;
+          margin: 0 0 17px 0;
           height: 34px;
           line-height: 34px;
 
-          p{
+          p {
             color: #f14242;
             font-size: 14px;
-            span{
+            span {
               font-size: 10px;
               color: #f14242;
             }
           }
-          >span{
+          > span {
             color: #999;
             font-size: 12px;
             position: relative;
-            top:-8px;
+            top: -8px;
             left: 0;
           }
         }
-        .topMiddle{
-          margin:0 auto;
+        .topMiddle {
+          margin: 0 auto;
           height: 34px;
-          .midTop{
+          .midTop {
             height: 16px;
             position: relative;
-            top:-10px;
+            top: -10px;
             margin-bottom: 7px;
-            span{
+            span {
               font-size: 16px;
               display: inline-block;;
-              &.icon{
+              &.icon {
                 vertical-align: middle;
                 width: 24px;
                 border-bottom: 2px solid #000;
@@ -206,25 +225,25 @@
               }
 
             }
-            img{
+            img {
               //float: left;
               position: relative;
-              top:-5px;
+              top: -5px;
               left: -7px;
             }
 
           }
-          .midBottom{
-            margin:0 auto;
+          .midBottom {
+            margin: 0 auto;
             position: relative;
-            top:-10px;
+            top: -10px;
             width: 233px;
             height: 17px;
-            span{
+            span {
               display: inline-block;
               color: #999;
               font-size: 12px;
-              &.fromTime{
+              &.fromTime {
                 margin-right: 60px;
               }
             }
@@ -233,21 +252,21 @@
         }
       }
 
-      .bottom{
+      .bottom {
         height: 33.5px;
         line-height: 33.5px;
         border-top: 1px dashed #e0e0e0;
-        font-size:12px;
+        font-size: 12px;
         color: #999;
         text-align: left;
 
-        p{
+        p {
           display: inline-block;
           height: 33.5px;
           line-height: 33.5px;
           margin-right: 18px;
         }
-        span{
+        span {
           color: #3cb687;
         }
       }
