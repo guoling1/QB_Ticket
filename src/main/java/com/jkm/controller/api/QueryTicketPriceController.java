637014beac05f374ae15ba565c72e32bb66151c5
Administrator <706326182@qq.com>
2016-11-07 16:52:40 +0800

    余票查询（有价格）业务

--- src/main/java/com/jkm/controller/api/QueryTicketPriceController.java
+++ src/main/java/com/jkm/controller/api/QueryTicketPriceController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+
 /**
  * Created by zhangbin on 2016/11/1.
  */
@@ -22,18 +24,35 @@ public class QueryTicketPriceController extends BaseController {
 
     @ResponseBody
     @RequestMapping(value = "/query", method = RequestMethod.POST)
-    public void query() throws Exception{
+    public JSONObject query(){
 
         JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String partnerid = HySdkConstans.QUERY_PARTNER_ID;
-        String method = "train_query";
-        String from_station = requestJson.getString("from_station");
-        String to_station = requestJson.getString("to_station");
-        String train_date = requestJson.getString("train_date");
-        String purpose_codes = "ADULT";
-        responseJson = this.queryTicketPriceService.queryTicket(partnerid, method, from_station, to_station, train_date, purpose_codes);
 
-        returnJson(responseJson);
+        JSONObject requestJson = null;
+        try {
+            requestJson = super.getRequestJsonParams();
+            String partnerid = HySdkConstans.QUERY_PARTNER_ID;
+            String method = "train_query";
+            String from_station = requestJson.getString("from_station");
+            String to_station = requestJson.getString("to_station");
+            String train_date = requestJson.getString("train_date");
+            String purpose_codes = "ADULT";
+            responseJson = this.queryTicketPriceService.queryTicket(partnerid, method, from_station, to_station, train_date, purpose_codes);
+
+            if(responseJson.getBoolean("success")) {
+                requestJson.put("success", true);
+                requestJson.put("code", responseJson.getLong("code"));
+                requestJson.put("msg", responseJson.getString("msg"));
+            }
+
+            responseJson.put("success", false);
+            responseJson.put("code",null);
+            responseJson.put("msg", responseJson.getString("msg"));
+        } catch (IOException e) {
+            responseJson.put("success", false);
+            responseJson.put("code",null);
+            responseJson.put("msg", "查询异常");
+        }
+        return responseJson;
     }
 }
