0f2d50b2766db25866f5b586624cb7374df50288
“xingliujie <java_xlj@163.com>
2016-11-09 17:38:43 +0800

    201611091602

--- src/main/java/com/jkm/controller/api/WebsiteController.java
+++ src/main/java/com/jkm/controller/api/WebsiteController.java
@@ -26,110 +26,110 @@ public class WebsiteController extends BaseController {
     @Autowired
     private WebsiteService websiteService;
 
-    /**
-     * 12306账号验证接口
-     * @return
-     * @throws Exception
-     */
-    @ResponseBody
-    @RequestMapping(value = "/validate", method = RequestMethod.POST)
-    public JSONObject validate() throws Exception {
-        JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String data = requestJson.getString("data");
-        String accountversion = requestJson.getString("accountversion");
-        data = DESUtil.encrypt(data);
-        JSONObject jo = new JSONObject();
-        jo.put("data",data);
-        jo.put("accountversion",accountversion);
-        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/cn_interface/trainAccount/validate");
-        return responseJson;
-    }
-    /**
-     * 查询常用联系人接口
-     * @return
-     * @throws Exception
-     */
-    @ResponseBody
-    @RequestMapping(value = "/query", method = RequestMethod.POST)
-    public JSONObject query() throws Exception {
-        JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String data = requestJson.getString("data");
-        String accountversion = requestJson.getString("accountversion");
-        data = DESUtil.encrypt(data);
-        JSONObject jo = new JSONObject();
-        jo.put("data",data);
-        jo.put("accountversion",accountversion);
-        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/cn_interface/trainAccount/contact/query");
-        String tempData = DESUtil.decrypt(responseJson.getString("data"));
-        responseJson.put("data",tempData);
-        return responseJson;
-    }
-    /**
-     * 增加和修改常用联系人接口
-     * @return
-     * @throws Exception
-     */
-    @ResponseBody
-    @RequestMapping(value = "/saveOrUpdate", method = RequestMethod.POST)
-    public JSONObject saveOrUpdate() throws Exception {
-        JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String data = requestJson.getString("data");
-        String accountversion = requestJson.getString("accountversion");
-        data = DESUtil.encrypt(data);
-        JSONObject jo = new JSONObject();
-        jo.put("data",data);
-        jo.put("accountversion",accountversion);
-        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/saveOrUpdate");
-        String tempData = DESUtil.decrypt(responseJson.getString("data"));
-        responseJson.put("data",tempData);
-        return responseJson;
-    }
-    /**
-     * 删除常用联系人接口
-     * @return
-     * @throws Exception
-     */
-    @ResponseBody
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public JSONObject delete() throws Exception {
-        JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String data = requestJson.getString("data");
-        String accountversion = requestJson.getString("accountversion");
-        data = DESUtil.encrypt(data);
-        JSONObject jo = new JSONObject();
-        jo.put("data",data);
-        jo.put("accountversion",accountversion);
-        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/delete");
-        String tempData = DESUtil.decrypt(responseJson.getString("data"));
-        responseJson.put("data",tempData);
-        return responseJson;
-    }
-
-    /**
-     * 增加和修改金开门网站常用联系人接口
-     * @return
-     * @throws Exception
-     */
-    @ResponseBody
-    @RequestMapping(value = "/saveOrUpdateOfJkm", method = RequestMethod.POST)
-    public JSONObject saveOrUpdateOfJkm() throws Exception {
-        JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        String data = requestJson.getString("data");
-        String accountversion = requestJson.getString("accountversion");
-        data = DESUtil.encrypt(data);
-        JSONObject jo = new JSONObject();
-        jo.put("data",data);
-        jo.put("accountversion",accountversion);
-        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/saveOrUpdate");
-        String tempData = DESUtil.decrypt(responseJson.getString("data"));
-        responseJson.put("data",tempData);
-        return responseJson;
-    }
+//    /**
+//     * 12306账号验证接口
+//     * @return
+//     * @throws Exception
+//     */
+//    @ResponseBody
+//    @RequestMapping(value = "/validate", method = RequestMethod.POST)
+//    public JSONObject validate() throws Exception {
+//        JSONObject responseJson = new JSONObject();
+//        JSONObject requestJson = super.getRequestJsonParams();
+//        String data = requestJson.getString("data");
+//        String accountversion = requestJson.getString("accountversion");
+//        data = DESUtil.encrypt(data);
+//        JSONObject jo = new JSONObject();
+//        jo.put("data",data);
+//        jo.put("accountversion",accountversion);
+//        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/cn_interface/trainAccount/validate");
+//        return responseJson;
+//    }
+//    /**
+//     * 查询常用联系人接口
+//     * @return
+//     * @throws Exception
+//     */
+//    @ResponseBody
+//    @RequestMapping(value = "/query", method = RequestMethod.POST)
+//    public JSONObject query() throws Exception {
+//        JSONObject responseJson = new JSONObject();
+//        JSONObject requestJson = super.getRequestJsonParams();
+//        String data = requestJson.getString("data");
+//        String accountversion = requestJson.getString("accountversion");
+//        data = DESUtil.encrypt(data);
+//        JSONObject jo = new JSONObject();
+//        jo.put("data",data);
+//        jo.put("accountversion",accountversion);
+//        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/cn_interface/trainAccount/contact/query");
+//        String tempData = DESUtil.decrypt(responseJson.getString("data"));
+//        responseJson.put("data",tempData);
+//        return responseJson;
+//    }
+//    /**
+//     * 增加和修改常用联系人接口
+//     * @return
+//     * @throws Exception
+//     */
+//    @ResponseBody
+//    @RequestMapping(value = "/saveOrUpdate", method = RequestMethod.POST)
+//    public JSONObject saveOrUpdate() throws Exception {
+//        JSONObject responseJson = new JSONObject();
+//        JSONObject requestJson = super.getRequestJsonParams();
+//        String data = requestJson.getString("data");
+//        String accountversion = requestJson.getString("accountversion");
+//        data = DESUtil.encrypt(data);
+//        JSONObject jo = new JSONObject();
+//        jo.put("data",data);
+//        jo.put("accountversion",accountversion);
+//        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/saveOrUpdate");
+//        String tempData = DESUtil.decrypt(responseJson.getString("data"));
+//        responseJson.put("data",tempData);
+//        return responseJson;
+//    }
+//    /**
+//     * 删除常用联系人接口
+//     * @return
+//     * @throws Exception
+//     */
+//    @ResponseBody
+//    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+//    public JSONObject delete() throws Exception {
+//        JSONObject responseJson = new JSONObject();
+//        JSONObject requestJson = super.getRequestJsonParams();
+//        String data = requestJson.getString("data");
+//        String accountversion = requestJson.getString("accountversion");
+//        data = DESUtil.encrypt(data);
+//        JSONObject jo = new JSONObject();
+//        jo.put("data",data);
+//        jo.put("accountversion",accountversion);
+//        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/delete");
+//        String tempData = DESUtil.decrypt(responseJson.getString("data"));
+//        responseJson.put("data",tempData);
+//        return responseJson;
+//    }
+//
+//    /**
+//     * 增加和修改金开门网站常用联系人接口
+//     * @return
+//     * @throws Exception
+//     */
+//    @ResponseBody
+//    @RequestMapping(value = "/saveOrUpdateOfJkm", method = RequestMethod.POST)
+//    public JSONObject saveOrUpdateOfJkm() throws Exception {
+//        JSONObject responseJson = new JSONObject();
+//        JSONObject requestJson = super.getRequestJsonParams();
+//        String data = requestJson.getString("data");
+//        String accountversion = requestJson.getString("accountversion");
+//        data = DESUtil.encrypt(data);
+//        JSONObject jo = new JSONObject();
+//        jo.put("data",data);
+//        jo.put("accountversion",accountversion);
+//        responseJson = HttpClientUtil.sendPost(jo,"http://trainorder.ws.hangtian123.com/trainAccount/contact/saveOrUpdate");
+//        String tempData = DESUtil.decrypt(responseJson.getString("data"));
+//        responseJson.put("data",tempData);
+//        return responseJson;
+//    }
 
     /**
      * 添加12306账号
@@ -140,22 +140,27 @@ public class WebsiteController extends BaseController {
     @RequestMapping(value = "/addWebSite", method = RequestMethod.POST)
     public JSONObject addWebSite() throws Exception {
         JSONObject responseJson = new JSONObject();
-        JSONObject requestJson = super.getRequestJsonParams();
-        Preconditions.checkNotNull(requestJson.get("data"),"缺失参数data");
-        Preconditions.checkNotNull(requestJson.get("uid"),"缺失参数uid");
-        Preconditions.checkNotNull(requestJson.get("appid"),"缺失参数appid");
-        String data = requestJson.getString("data");
-        String uid = requestJson.getString("uid");
-        String appid = requestJson.getString("appid");
-        long backId = websiteService.addWebSite(data,uid,appid);
-        if(backId>0){
-            responseJson.put("result", true);
-            responseJson.put("data",backId);
-            responseJson.put("message", "登录成功");
-        }else{
+        try{
+
+            JSONObject requestJson = super.getRequestJsonParams();
+            Preconditions.checkNotNull(requestJson.get("data"),"缺失参数data");
+            Preconditions.checkNotNull(requestJson.get("uid"),"缺失参数uid");
+            Preconditions.checkNotNull(requestJson.get("appid"),"缺失参数appid");
+            String data = requestJson.getString("data");
+            String uid = requestJson.getString("uid");
+            String appid = requestJson.getString("appid");
+            long backId = websiteService.addWebSite(data,uid,appid);
+            if(backId>0){
+                responseJson.put("result", true);
+                responseJson.put("data",backId);
+                responseJson.put("message", "登录成功");
+            }else{
+                responseJson.put("result", false);
+                responseJson.put("message", "登录失败");
+            }
+        }catch(Exception e){
             responseJson.put("result", false);
-            responseJson.put("data", null);
-            responseJson.put("message", "登录失败");
+            responseJson.put("message", e.getMessage());
         }
         return responseJson;
     }
@@ -175,11 +180,10 @@ public class WebsiteController extends BaseController {
             responseJson.put("result", true);
             responseJson.put("data",null);
             responseJson.put("message", "导入成功");
-        } catch (IOException e) {
+        } catch (Exception e) {
             log.info("导入失败");
             responseJson.put("result", false);
-            responseJson.put("data",null);
-            responseJson.put("message", "导入失败");
+            responseJson.put("message", e.getMessage());
         }
         return responseJson;
     }
