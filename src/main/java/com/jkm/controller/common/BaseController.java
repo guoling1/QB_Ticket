ef935b709728d16763aea7a9aafdcb875f440e55
Administrator <706326182@qq.com>
2016-11-09 18:20:49 +0800

    银行卡归属

--- src/main/java/com/jkm/controller/common/BaseController.java
+++ src/main/java/com/jkm/controller/common/BaseController.java
@@ -1,8 +1,6 @@
 package com.jkm.controller.common;
 
 import com.google.common.base.Preconditions;
-import com.jkm.controller.helper.ResponseEntityBase;
-import com.jkm.entity.BankCardBin;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -16,7 +14,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * 控制层基类
@@ -125,7 +124,7 @@ public class BaseController {
      * @param data
      * @throws IOException
      */
-    public void returnJson(ResponseEntityBase<BankCardBin> data) throws IOException {
+    public void returnJson(JSONObject data) throws IOException {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/json;charset=UTF-8");
         response.setHeader("cache-control", "max-age=7200");
