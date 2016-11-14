af8a0bcffc10eab9b9678447ca6dff410f16a746
“xingliujie <java_xlj@163.com>
2016-11-14 10:27:39 +0800

    201611101640

--- src/main/java/com/jkm/util/MQProducer4Spring.java
+++ src/main/java/com/jkm/util/MQProducer4Spring.java
@@ -6,6 +6,7 @@ import com.aliyun.openservices.ons.api.SendResult;
 import com.aliyun.openservices.ons.api.bean.ProducerBean;
 import com.jkm.entity.fusion.QueryRefundData;
 import com.jkm.util.mq.MqConfig;
+import com.jkm.util.mq.MqProducer;
 import net.sf.json.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -22,11 +23,13 @@ public class MQProducer4Spring {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("producer.xml");
         ProducerBean producer = (ProducerBean) ctx.getBean("producers");
         System.out.println("Producer Started");
+
         JSONObject mqJo = new JSONObject();
-
-
-            mqJo.put("reqSn","11111");
-            Message message = new Message(MqConfig.TOPIC, MqConfig.SINGLE_REFUND_QUERY, mqJo.toString().getBytes("UTF8"));
+        mqJo.put("reqSn","20161111132457210572");
+        mqJo.put("dt", "20161111");
+        mqJo.put("sendCount",0);
+        mqJo.put("orderId",165);
+            Message message = new Message(MqConfig.TOPIC, MqConfig.FAST_PAY_QUERY, mqJo.toString().getBytes("UTF8"));
             SendResult sendResult = producer.send(message);
             if (sendResult != null) {
                 System.out.println(new Date() + " Send mq message success! Topic is:" + MqConfig.TOPIC + "msgId is: " + sendResult.getMessageId());
