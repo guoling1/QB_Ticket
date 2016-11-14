962950683e107637c0ea9b11b1f2c745b89db81e
于超 <chao.yu@jinkaimen.com>
2016-11-14 21:06:20 +0800

    对接接口

--- src/main/webapp/QB_app/src/components/ticket/Order.vue
+++ src/main/webapp/QB_app/src/components/ticket/Order.vue
@@ -1,22 +1,47 @@
 <template lang="html">
   <div class="main">
-    <div class="reserve show">预订单</div>
-    <div class="rob">抢票单</div>
-    <ul>
-      <li v-for="massage in massages">
+    <div class="reserve" v-bind:class="{show:preShow}" @click="teb(1)">预订单</div>
+    <div class="rob" v-bind:class="{show:robShow}" @click="teb(2)">抢票单</div>
+    <ul v-show="preShow">
+      <li v-for="massage in preMassages">
         <div class="top">
-          <span class="date">{{massage.startDate}} {{massage.startTime}}出发</span><span class="checi">{{massage.checi}}</span>
+          <span class="date">{{massage.startDate}} {{massage.startTime}}出发</span><span
+          class="checi">{{massage.checi}}</span>
         </div>
         <div class="bottom">
           <div>
             <span class="form">{{massage.fromStationName}}</span>
-            <img src="../../assets/jiantou.png" alt="" />
+            <img src="../../assets/jiantou.png" alt=""/>
             <span class="to">{{massage.toStationName}}</span>
           </div>
           <span class="name" v-for="passenger in massage.passengers">{{passenger.name}}</span>
           <span class="price" v-for="passenger in massage.passengers">￥{{passenger.price}}</span>
           <router-link :to="{path:'/ticket/refund-detail',query:{orderFormId:massage.orderFormId}}">
-            <p class="static"  v-for="passenger in massage.passengers" >{{passengerStatus[passenger.status-1]}}<p>
+            <p class="static" v-for="passenger in massage.passengers">{{passengerStatus[passenger.status-1]}}
+
+            <p>
+          </router-link>
+        </div>
+      </li>
+    </ul>
+    <ul v-show="robShow">
+      <li v-for="massage in robMassages">
+        <div class="top">
+          <span class="date">{{massage.startDate}} {{massage.startTime}}出发</span><span
+          class="checi">{{massage.checi}}</span>
+        </div>
+        <div class="bottom">
+          <div>
+            <span class="form">{{massage.fromStationName}}</span>
+            <img src="../../assets/jiantou.png" alt=""/>
+            <span class="to">{{massage.toStationName}}</span>
+          </div>
+          <span class="name" v-for="passenger in massage.passengers">{{passenger.name}}</span>
+          <span class="price" v-for="passenger in massage.passengers">￥{{passenger.price}}</span>
+          <router-link :to="{path:'/ticket/refund-detail',query:{orderFormId:massage.orderFormId}}">
+            <p class="static" v-for="passenger in massage.passengers">{{passengerStatus[passenger.status-1]}}
+
+            <p>
           </router-link>
         </div>
       </li>
@@ -30,25 +55,58 @@
     name: 'menu',
     data () {
       return {
-        massages: [],
-        passengerStatus:["票初始化","出票成功","出票失败","退票中","退票请求成功","退票成功","退票失败","订单取消"],
-        orderStatus:["订单已删除"," ","订单初始化","占座申请中","占座成功","占座失败","支付中","客户付款成功","客户付款失败","确认出票请求失败","确认出票请求成功","出票成功","出票失败","订单已经退票","订单取消","退票中","退票成功","退票失败"],
+        common: {
+          appid: '',
+          uid: ''
+        },
+        preMassages: false,
+        preShow: true,
+        robMassages: false,
+        robShow: false,
+        passengerStatus: ["票初始化", "出票成功", "出票失败", "退票中", "退票请求成功", "退票成功", "退票失败", "订单取消"],
+        orderStatus: ["订单已删除", " ", "订单初始化", "占座申请中", "占座成功", "占座失败", "支付中", "客户付款成功", "客户付款失败", "确认出票请求失败", "确认出票请求成功", "出票成功", "出票失败", "订单已经退票", "订单取消", "退票中", "退票成功", "退票失败"],
+      }
+    },
+    methods: {
+      teb: function (code) {
+        if (code == 1) {
+          this.$data.preShow = true;
+          this.$data.robShow = false;
+        } else {
+          this.$data.preShow = false;
+          this.$data.robShow = true;
+          if (!this.$data.robMassages) {
+            this.$http.post('/order/queryGrabOrder', {
+              grabTicketFormId: 0,
+              appid: this.$data.common.appid,
+              uid: this.$data.common.uid
+            }).then(function (res) {
+              console.log(res);
+            }, function (err) {
+              console.log(err);
+            })
+          }
+        }
       }
     },
     beforeRouteEnter (to, from, next) {
-      Vue.http.post('/order/queryMyOrder',{"uid": "123456"})
-        .then(function (response) {
-          next(vm=> {
-            if(response.data.code==1){
-              vm.$data.massages = response.data.data;
-            }
-          })
+      Vue.http.post('/order/queryMyOrder', {
+        appid: to.query.appid,
+        uid: to.query.uid
+      }).then(function (response) {
+        next(vm=>{
+          vm.$data.common.appid = to.query.appid;
+          vm.$data.common.uid = to.query.uid;
+          if (response.data.code == 1) {
+            vm.$data.preMassages = response.data.data;
+          }
         })
-        .catch(function (response) {
-          next(false)
-        })
+      }).catch(function (err) {
+        console.log(err);
+        next(false)
+      })
     }
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -65,34 +123,34 @@
     width: 100%;
     height: 100%;
     overflow: hidden;
-    background:#f5f5f5;
-    .flexItem(1, 100 %);
+    background: #f5f5f5;
+    .flexItem(1, 100%);
     overflow: hidden;
 
-    .reserve,.rob{
-      width:50%;
-      height:44px;
-      color:#ccc;
+    .reserve, .rob {
+      width: 50%;
+      height: 44px;
+      color: #ccc;
       background: #fff;
-      font-size:15px;
+      font-size: 15px;
       text-align: center;
       line-height: 44px;
-      float:left;
+      float: left;
     }
 
-    .show{
-      color:#4ab9f1;
+    .show {
+      color: #4ab9f1;
     }
 
-    ul{
+    ul {
       margin-top: 49px;
 
-      li{
+      li {
         height: 113px;
         background: #fff;
         margin-top: 5px;
 
-        .top{
+        .top {
           font-size: 14px;
           height: 37px;
           line-height: 37px;
@@ -100,55 +158,55 @@
           padding-left: 17px;
           padding-right: 17px;
 
-          .date{
+          .date {
             float: left;
           }
-          .checi{
+          .checi {
             float: right;
           }
         }
 
-        .bottom{
+        .bottom {
           font-size: 18px;
           padding: 17px 17px 10px;
           position: relative;
           overflow: hidden;
 
-          div{
+          div {
             width: 183px;
             margin-bottom: 15px;
             overflow: hidden;
             font-weight: bold;
 
-            .form{
+            .form {
               float: left;
             }
 
-            .to{
+            .to {
               float: right;
             }
 
-            img{
+            img {
               vertical-align: 5px;
             }
           }
 
-          .name{
+          .name {
             float: left;
             font-size: 12px;
             color: #999;
             margin-right: 8px;
           }
 
-          .price{
+          .price {
             color: #f14242;
             font-weight: bold;
-            position:absolute;
+            position: absolute;
             top: 18px;
             right: 17px;
           }
 
-          .static{
+          .static {
             position: absolute;
             bottom: 10px;
             right: 15px;
