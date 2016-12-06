5af48c91bb5552c22aeb441a73d9e5a838c53f27
lt <yjshsszlt@163.com>
2016-12-06 17:23:24 +0800

    验参

--- src/main/java/com/jkm/interceptor/BaseControllerInterceptor.java
+++ src/main/java/com/jkm/interceptor/BaseControllerInterceptor.java
@@ -4,8 +4,6 @@ package com.jkm.interceptor;
 import com.jkm.controller.common.BaseController;
 import com.jkm.entity.MerchantAppInfo;
 import com.jkm.service.MerchantAppInfoService;
-import com.jkm.util.DateFormatUtil;
-import com.jkm.util.MD5Util;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.method.HandlerMethod;
@@ -13,7 +11,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
 /**
  * 描述：<br>
@@ -28,30 +25,30 @@ public class BaseControllerInterceptor extends HandlerInterceptorAdapter {
 			HttpServletResponse response, Object handler) throws Exception {
 		boolean flag = true;
 
-		String current = "";
-		String appId = "";
-
 		if(request.getMethod().toUpperCase().equals("POST")){//防止篡改url地址
 			String url = request.getHeader("Referer");
 			if(url!=null&&url.contains("appid")){
-				current = url.substring(url.indexOf("?")+1,url.length());
+				String current = url.substring(url.indexOf("?")+1,url.length());
 				String[] arr = current.split("&");
 				for(int i=0;i<arr.length;i++){
 					String[] param = arr[i].split("=");
 					if("appid".equals(param[0])){
-						appId = param[1];
+						String appId = param[1];
 						MerchantAppInfo merchantAppInfo = merchantAppInfoService.selectByOpenId(appId);
 						if(merchantAppInfo==null){
 							response.sendError(506,"非法数据、未通过验证");
 							flag = false;
 						}
+						/*String secretKey = merchantAppInfoService.selectSecretKeyByOpenId(appId);
+						String uid = request.getParameter("uid");
+						String sign = MD5Util.MD5(appId + uid + reqTime + MD5Util.MD5(secretKey));*/
 					}
 					if(!flag){
 						break;
 					}
 				}
 
-				String uid = request.getParameter("uid");
+				/*String uid = request.getParameter("uid");
 				String reqTime = DateFormatUtil.format(new Date(),"yyyyMMddHHmmss");
 				String key = merchantAppInfoService.selectSecretKeyByOpenId(appId);
 				String sign = MD5Util.MD5(appId + uid + reqTime + MD5Util.MD5(key));
@@ -59,7 +56,7 @@ public class BaseControllerInterceptor extends HandlerInterceptorAdapter {
 						"appid=" + appId + "&" +
 						"uid=" + uid + "&" +
 						"reqTime=" + reqTime + "&" +
-						"sign=" + sign);
+						"sign=" + sign);*/
 			}
 		}
 		if(!flag){
