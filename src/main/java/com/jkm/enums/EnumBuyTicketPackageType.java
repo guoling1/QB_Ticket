4c2e106044c0d6168b10c64762a522a8bf8689c7
xiang.yu <http://gitlab.jinkaimen.com>
2016-12-14 14:22:39 +0800

    bug

--- src/main/java/com/jkm/enums/EnumBuyTicketPackageType.java
+++ src/main/java/com/jkm/enums/EnumBuyTicketPackageType.java
@@ -13,9 +13,9 @@ public enum EnumBuyTicketPackageType {
 
     TICKET_PACKAGE_FIRST(1, "0元套餐", "0"),
 
-    TICKET_PACKAGE_SECOND(2, "20元套餐", "0.20"),
+    TICKET_PACKAGE_SECOND(2, "20元套餐", "20"),
 
-    TICKET_PACKAGE_THIRD(3, "30元套餐", "0.30");
+    TICKET_PACKAGE_THIRD(3, "30元套餐", "30");
 
     @Getter
     private int id;
